10d19a905113c655aee2d25cdef0d5ad1d99bb98
Tehran Bayramov <nicosspyros@gmail.com>
2024-11-06 22:54:00 +0400

    dsfgsd

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -51,10 +51,10 @@ const Footer = () => {
           {/* Contact Information */}
           <Elaqe
             className="w-full px-4 sm:w-1/2 md:w-1/3 lg:w-3/12"
-            elaqe="Əlaqə"
+            elaqe="İletişim"
           />
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3039.464245205028!2d49.827639675093096!3d40.37640235809423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307dbc42dba4a1%3A0x21b2b3488e89a7!2s197%20Suleyman%20Rahimov%20St%2C%20Baku%201009!5e0!3m2!1str!2saz!4v1728909374648!5m2!1str!2saz"
+            src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3012.019446002714!2d28.730899075723975!3d40.98105502110124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caa1a5b858cda9%3A0xf7a03300992e422d!2zR8O8bcO8xZ9wYWxhLCBNZXN1dCBTay4gTm86NywgMzQzMjAgQXZjxLFsYXIvxLBzdGFuYnVsLCBUw7xya2l5ZQ!5e0!3m2!1str!2saz!4v1730919131789!5m2!1str!2saz"
             width="auto"
             height="auto"
             style={{ border: "0", paddingBottom: "10px" }}
@@ -99,7 +99,7 @@ const Footer = () => {
           </p>
         </div>
       </div>
-    </footer>
+    </footer >
   );
 };
 
