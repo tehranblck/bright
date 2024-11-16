27667fda30a72dd1249abc1e992984acc5a29869
Tehran Bayramov <nicosspyros@gmail.com>
2024-11-16 22:27:31 +0400

    whatsapp addedd

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const ProductCard = ({ image }: { image: string }) => {
     return (
         <div className="bg-white dark:bg-[#1E201E] rounded-lg shadow-md overflow-hidden">
-            <Image
-                src={image}
-                alt="Poster"
-                className="w-full h-auto object-cover"
-                width={300}
-                height={300}
-            />
+            <Link href="https://wa.me/+905513954420">
+                <Image
+                    src={image}
+                    alt="Poster"
+                    className="w-full h-auto object-cover"
+                    width={300}
+                    height={300}
+                />
+            </Link>
         </div>
     );
 };
